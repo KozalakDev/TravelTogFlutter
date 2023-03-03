e331192d65f5f81c5ca6537f70de8031008f723b
mooncake <59047525+ferhatAksoy@users.noreply.github.com>
2023-03-03 18:02:22 +0300

    overflow error fixed

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -14,29 +14,43 @@ class LoginScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Color.fromRGBO(250, 250, 250, 1),
-        body: Column(
-          children: [
-            Image.asset(
-              "assets/images/ttog.png",
-              scale: 1.8,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Text("Sign In",
-                          style: GoogleFonts.montserrat(
-                              color: Color.fromRGBO(245, 127, 90, 1),
-                              fontSize: 40)),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    child: TextFormField(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/ttog.png",
+                scale: 1.8,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text("Sign In",
+                            style: GoogleFonts.montserrat(
+                                color: Color.fromRGBO(245, 127, 90, 1),
+                                fontSize: 40)),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            hintText: "E-mail",
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                borderSide: BorderSide(
+                                    color: Color.fromRGBO(196, 196, 196, 1))),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color.fromRGBO(196, 196, 196, 1)),
+                                borderRadius: BorderRadius.circular(15))),
+                      ),
+                    ),
+                    TextFormField(
                       decoration: InputDecoration(
-                          hintText: "E-mail",
+                          hintText: "Password",
                           enabledBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(15),
                               borderSide: BorderSide(
@@ -46,96 +60,84 @@ class LoginScreen extends StatelessWidget {
                                   color: Color.fromRGBO(196, 196, 196, 1)),
                               borderRadius: BorderRadius.circular(15))),
                     ),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        hintText: "Password",
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15),
-                            borderSide: BorderSide(
-                                color: Color.fromRGBO(196, 196, 196, 1))),
-                        focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                                color: Color.fromRGBO(196, 196, 196, 1)),
-                            borderRadius: BorderRadius.circular(15))),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    width: MediaQuery.of(context).size.width / 1.3,
-                    child: RoundedLoadingButton(
-                      duration: Duration(seconds: 3),
-                      color: Color.fromRGBO(245, 127, 90, 1),
-                      controller: RoundedLoadingButtonController(),
-                      onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                          return HomeScreen();
-                        }));
-                      },
-                      child: Text(
-                        "Sign In",
-                        style: TextStyle(fontSize: 16),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      width: MediaQuery.of(context).size.width / 1.3,
+                      child: RoundedLoadingButton(
+                        duration: Duration(seconds: 3),
+                        color: Color.fromRGBO(245, 127, 90, 1),
+                        controller: RoundedLoadingButtonController(),
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return HomeScreen();
+                          }));
+                        },
+                        child: Text(
+                          "Sign In",
+                          style: TextStyle(fontSize: 16),
+                        ),
                       ),
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Forgot Password"),
-                      TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          "Reset Password",
-                          style: GoogleFonts.montserrat(
-                              color: Color.fromRGBO(248, 152, 128, 1),
-                              fontWeight: FontWeight.w600),
-                        ),
-                      )
-                    ],
-                  ),
-                  SignInButton(
-                    Buttons.google,
-                    onPressed: () {},
-                    shape: OutlineInputBorder(
-                        borderSide: BorderSide(width: 0.3),
-                        borderRadius: BorderRadius.circular(20)),
-                  ),
-                  SignInButton(
-                    Buttons.apple,
-                    onPressed: () {},
-                    shape: OutlineInputBorder(
-                        borderSide: BorderSide(width: 0.3),
-                        borderRadius: BorderRadius.circular(20)),
-                  ),
-                  SignInButton(
-                    Buttons.facebookNew,
-                    onPressed: () {},
-                    shape: OutlineInputBorder(
-                        borderSide: BorderSide(width: 0.3),
-                        borderRadius: BorderRadius.circular(20)),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("You didn't"),
-                      TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          "sign up ?",
-                          style: GoogleFonts.montserrat(
-                              color: Color.fromRGBO(248, 152, 128, 1),
-                              fontWeight: FontWeight.w600),
-                        ),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            )
-          ],
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Forgot Password"),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Reset Password",
+                            style: GoogleFonts.montserrat(
+                                color: Color.fromRGBO(248, 152, 128, 1),
+                                fontWeight: FontWeight.w600),
+                          ),
+                        )
+                      ],
+                    ),
+                    SignInButton(
+                      Buttons.google,
+                      onPressed: () {},
+                      shape: OutlineInputBorder(
+                          borderSide: BorderSide(width: 0.3),
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                    SignInButton(
+                      Buttons.apple,
+                      onPressed: () {},
+                      shape: OutlineInputBorder(
+                          borderSide: BorderSide(width: 0.3),
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                    SignInButton(
+                      Buttons.facebookNew,
+                      onPressed: () {},
+                      shape: OutlineInputBorder(
+                          borderSide: BorderSide(width: 0.3),
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("You didn't"),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "sign up ?",
+                            style: GoogleFonts.montserrat(
+                                color: Color.fromRGBO(248, 152, 128, 1),
+                                fontWeight: FontWeight.w600),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
